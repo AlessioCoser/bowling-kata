9312bc904fd0f17d3546c12e22707372923d3437
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-09 16:52:34 +0100

    create file structures for a maven project

---
+++ src/test/java/com/xpeppers/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package com.xpeppers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BowlingGameTest {
+    @Test
+    public void assert_true() {
+        assertTrue(true);
+    }
+}
